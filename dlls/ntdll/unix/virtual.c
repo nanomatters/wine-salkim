209d8fa6381b00019671113218afb71b1076bf3a
Andrew Wesie <awesie@gmail.com>
2020-04-24 14:55:17 -0500

    ntdll: Report unmodified WRITECOPY pages as shared.
    We also need to clear the modified bit after we clear memory in map_image to
    match the behavior of Windows.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=48665
    Signed-off-by: Andrew Wesie <awesie@gmail.com>

--- dlls/ntdll/unix/virtual.c
+++ dlls/ntdll/unix/virtual.c
@@ -5923,7 +5923,7 @@ static void fill_working_set_info( struct fill_working_set_info_data *d, struct 
         pagemap = d->pm_buffer[page - d->buffer_start];
 
         p->VirtualAttributes.Valid = !(vprot & VPROT_GUARD) && (vprot & 0x0f) && (pagemap >> 63);
-        p->VirtualAttributes.Shared = !is_view_valloc( view ) && ((pagemap >> 61) & 1);
+        p->VirtualAttributes.Shared = (!is_view_valloc( view ) && ((pagemap >> 61) & 1)) || ((view->protect & VPROT_WRITECOPY) && !(vprot & VPROT_COPIED));
         if (p->VirtualAttributes.Shared && p->VirtualAttributes.Valid)
             p->VirtualAttributes.ShareCount = 1; /* FIXME */
         if (p->VirtualAttributes.Valid)
