781b96db990d6417588e27118a77f580cf6a5051
Cameron Moore <moore.cameron1111@gmail.com>
2024-02-03 21:21:50 -0500

    user32: [GAMEFIX] Fix xinput support for Dragon Age Inquisition
    user32: [GAMEFIX] 1/2 Dragon Age Inquisition: fix xinput support
    
    https://bugs.winehq.org/show_bug.cgi?id=47070
    
    I'm not completely sure on the specifics of why the bug happens,
    but there are two main threads involved. Thread A owns the window
    object for the game. Thread B makes all the calls to XInputGetState.
    However, Thread B checks if the window has focus to receive keyboard
    inputs every frame. Thread B calls AttachThreadInput to attach its
    message queue to the Thread A's window a few times before the bug
    starts occurring along with one separate time at the start where
    Thread A attaches Thread B's message queue to the window as well.
    The message queue is detached one last time and then Thread B no
    longer has access to the window.
    
    My workaround checks GetFocus for when it starts receiving Null
    for a response (meaning it does not have access to a window anymore).
    Prior to this, AttachThreadInput stores the IDs of the two threads
    which call the function the very first time (hopefully this works
    with the EA App as I've been debugging this with Origin which doesn't
    seem to call either of the two WINAPI functions as long as you load
    straight into the game from the Origin library menu). When GetFocus
    realizes it does not have a window for the thread, it calls
    AttachThreadInput with the two thread IDs. This allows Thread B to
    access the window, which let's GetFocus return the proper window
    handle, which then let's the program access XInputGetState meaning
    the controller finally works.
    
    Anyway, as far as the final cause of the bug: it might be because
    of thread prioritization causing Thread B to AttachThreadInput to
    be called by it last as the only time Thread A calls it, it does
    not detach the message queue afterwards. This leads me to believe
    that the bug might just be caused by incomplete wine
    functionality / stubs. Although, this part is speculation.
    
    user32: [GAMEFIX] 2/2 Dragon Age Inquisition: fix xinput support
    
    Wrap hack with a SteamGameId check and cache it.

--- dlls/user32/input.c
+++ dlls/user32/input.c
@@ -814,8 +814,28 @@ HWND WINAPI GetActiveWindow(void)
 HWND WINAPI GetFocus(void)
 {
     GUITHREADINFO info;
+    HWND retValueWindow;
+    static HWND prev = 0;
+    const char *sgi;
+    static int is_DragonAgeInquis = -1;
+
     info.cbSize = sizeof(info);
-    return NtUserGetGUIThreadInfo( GetCurrentThreadId(), &info ) ? info.hwndFocus : 0;
+
+    retValueWindow = NtUserGetGUIThreadInfo( GetCurrentThreadId(), &info ) ? info.hwndFocus : 0;
+
+    if (is_DragonAgeInquis)
+    {
+        if ((is_DragonAgeInquis == 1) ||
+            (is_DragonAgeInquis = ((sgi = getenv("SteamGameId")) && !strcmp(sgi, "1222690"))))
+        {
+            if (retValueWindow == 0 && prev != 0)
+                NtUserAttachThreadInput(0, 0, 1);
+            else
+                prev = retValueWindow;
+        }
+    }
+
+    return retValueWindow;
 }
 
 
