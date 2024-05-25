e22cc1f35eb56818357c443dd034751712732c31
Stelios Tsampas <loathingkernel@gmail.com>
2024-05-25 20:56:03 +0300

    GAMEFIX: ntdll: file search workaround for Phantasy Star Online 2.

--- dlls/ntdll/unix/file.c
+++ dlls/ntdll/unix/file.c
@@ -3614,6 +3614,7 @@ static NTSTATUS lookup_unix_name( const WCHAR *name, int name_len, char **buffer
     struct stat st;
     char *unix_name = *buffer;
     const WCHAR *ptr, *end;
+    static char *skip_search = NULL;
 
     /* check syntax of individual components */
 
@@ -3660,6 +3661,13 @@ static NTSTATUS lookup_unix_name( const WCHAR *name, int name_len, char **buffer
     if (is_unix && (disposition == FILE_OPEN || disposition == FILE_OVERWRITE))
         return STATUS_OBJECT_NAME_NOT_FOUND;
 
+    if (skip_search == NULL)
+    {
+        skip_search = getenv("WINE_NO_OPEN_FILE_SEARCH");
+        WARN("Disabling case insensitive search for opening files");
+    }
+    if (skip_search && strcasestr(unix_name, skip_search) && disposition == FILE_OPEN)
+        return STATUS_OBJECT_NAME_NOT_FOUND;
     /* now do it component by component */
 
     while (name_len)
