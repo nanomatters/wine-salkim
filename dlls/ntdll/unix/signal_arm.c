b24dbc6222c73b60c5c4f8cca66d81dc04a1cc24
Torge Matthies <openglfreak@googlemail.com>
2024-09-11 20:42:20 +0300

    ntdll: fix compilation with link-time optimizations

--- dlls/ntdll/unix/signal_arm.c
+++ dlls/ntdll/unix/signal_arm.c
@@ -1631,7 +1631,7 @@ void signal_init_process(void)
 /***********************************************************************
  *           call_init_thunk
  */
-void call_init_thunk( LPTHREAD_START_ROUTINE entry, void *arg, BOOL suspend, TEB *teb,
+__attribute__((used)) void call_init_thunk( LPTHREAD_START_ROUTINE entry, void *arg, BOOL suspend, TEB *teb,
                       struct syscall_frame *frame, void *syscall_cfa )
 {
     struct arm_thread_data *thread_data = (struct arm_thread_data *)&teb->GdiTebBatch;
