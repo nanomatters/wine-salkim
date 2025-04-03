tannisroot/wine-1

mshtml: Improve IOleInPlaceActiveObject TranslateAccelerator
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=37058

dlls/mshtml/oleobj.c
@@ -1021,8 +1021,28 @@ static HRESULT WINAPI OleInPlaceActiveObject_ContextSensitiveHelp(IOleInPlaceAct
 static HRESULT WINAPI OleInPlaceActiveObject_TranslateAccelerator(IOleInPlaceActiveObject *iface, LPMSG lpmsg)
 {
     HTMLDocument *This = impl_from_IOleInPlaceActiveObject(iface);
-    FIXME("(%p)->(%p)\n", This, lpmsg);
-    return E_NOTIMPL;
+    HRESULT hres = S_FALSE;
+
+    TRACE("(%p)->(%p)\n", This, lpmsg);
+
+    switch(lpmsg->message)
+    {
+        case WM_KEYDOWN:
+            break;
+        case WM_KEYUP:
+        {
+            TRACE("Processing key %ld\n", lpmsg->wParam);
+            if (lpmsg->wParam == VK_F5)
+                hres = IOleCommandTarget_Exec(&This->IOleCommandTarget_iface, NULL, OLECMDID_REFRESH, 0, NULL, NULL);
+
+            break;
+        }
+        default:
+            FIXME("Unsupported message %04x\n", lpmsg->message);
+    }
+
+    TRACE("result 0x%08x\n", hres);
+    return hres;
 }
 
 static HRESULT WINAPI OleInPlaceActiveObject_OnFrameWindowActivate(IOleInPlaceActiveObject *iface,

nanomatters/wine-salkim

dlls/mshtml/oleobj.c
@@ -1392,8 +1392,28 @@ static HRESULT WINAPI DocObjOleInPlaceActiveObject_ContextSensitiveHelp(IOleInPl
 static HRESULT WINAPI DocObjOleInPlaceActiveObject_TranslateAccelerator(IOleInPlaceActiveObject *iface, LPMSG lpmsg)
 {
     HTMLDocumentObj *This = HTMLDocumentObj_from_IOleInPlaceActiveObject(iface);
-    FIXME("(%p)->(%p)\n", This, lpmsg);
-    return E_NOTIMPL;
+    HRESULT hres = S_FALSE;
+
+    TRACE("(%p)->(%p)\n", This, lpmsg);
+
+    switch(lpmsg->message)
+    {
+        case WM_KEYDOWN:
+            break;
+        case WM_KEYUP:
+        {
+            TRACE("Processing key %Ix\n", lpmsg->wParam);
+            if (lpmsg->wParam == VK_F5)
+                hres = IOleCommandTarget_Exec(&This->IOleCommandTarget_iface, NULL, OLECMDID_REFRESH, 0, NULL, NULL);
+
+            break;
+        }
+        default:
+            FIXME("Unsupported message %04x\n", lpmsg->message);
+    }
+
+    TRACE("result 0x%08lx\n", hres);
+    return hres;
 }
 
 static HRESULT WINAPI DocObjOleInPlaceActiveObject_OnFrameWindowActivate(IOleInPlaceActiveObject *iface,