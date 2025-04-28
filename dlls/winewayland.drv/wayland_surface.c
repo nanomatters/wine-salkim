84fedc49663bd62ef9ab259d2022c1e34539d49c
Zebra2711 <zebra2711@users.noreply.github.com>
2025-04-28 16:21:24 +0300

    winewayland.drv: Fill wayland app id based on Steam appid

--- dlls/winewayland.drv/wayland_surface.c
+++ dlls/winewayland.drv/wayland_surface.c
@@ -333,8 +333,20 @@ void wayland_surface_make_toplevel(struct wayland_surface *surface, BOOL server_
     if (!surface->xdg_toplevel) goto err;
     xdg_toplevel_add_listener(surface->xdg_toplevel, &xdg_toplevel_listener, surface->hwnd);
 
-    if (process_name)
-        xdg_toplevel_set_app_id(surface->xdg_toplevel, process_name);
+    static char steam_proton[] = "steam_proton";
+    const char *app_id = getenv("SteamAppId");
+    char proton_app_class[128];
+
+    if(!app_id || !*app_id) {
+        app_id = getenv("WINE_WMCLASS");
+    }
+
+    if (app_id && *app_id) {
+        snprintf(proton_app_class, sizeof(proton_app_class), "steam_app_%s", app_id);
+        xdg_toplevel_set_app_id(surface->xdg_toplevel, proton_app_class);
+    } else {
+        xdg_toplevel_set_app_id(surface->xdg_toplevel, steam_proton);
+    }
 
     if (process_wayland.xdg_toplevel_tag_manager_v1)
     {
