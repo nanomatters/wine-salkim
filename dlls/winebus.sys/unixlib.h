b4d017be8971ba7ebbd3059ae751b5b8a143aaa5
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2025-05-19 20:33:06 -0400

    winebus: Fix PROTON_ENABLE/DISABLE_HIDRAW.

--- dlls/winebus.sys/unixlib.h
+++ dlls/winebus.sys/unixlib.h
@@ -126,6 +126,14 @@ struct device_report_params
     IO_STATUS_BLOCK *io;
 };
 
+struct hidraw_enabled_params
+{
+    unsigned short vid;
+    unsigned short pid;
+    BOOL env_set;
+    BOOL enabled;
+};
+
 enum unix_funcs
 {
     sdl_init,
@@ -145,6 +153,7 @@ enum unix_funcs
     device_set_output_report,
     device_get_feature_report,
     device_set_feature_report,
+    hidraw_enabled,
     unix_funcs_count,
 };
 
