994498022582b4c7993b105f4ed8cce92cf96727
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2025-06-02 15:53:52 -0400

    winewayland: ensure flush when setting fullscreen state.

--- dlls/winewayland.drv/wayland_surface.c
+++ dlls/winewayland.drv/wayland_surface.c
@@ -711,6 +711,7 @@ static void wayland_surface_reconfigure_geometry(struct wayland_surface *surface
                     xdg_toplevel_unset_fullscreen(surface->xdg_toplevel);
                     wl_display_flush(process_wayland.wl_display);
                     xdg_toplevel_set_fullscreen(surface->xdg_toplevel, output);
+                    wl_display_flush(process_wayland.wl_display);
                     /* In case we don't get enter event from compositor
                     happens on sway for instance
                     */
