3efbb711cef58cae5b480aff939526ef04524e4e
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2025-06-12 17:30:25 -0400

    winewayland: small cleanups

--- dlls/winewayland.drv/window_surface.c
+++ dlls/winewayland.drv/window_surface.c
@@ -535,12 +535,13 @@ BOOL WAYLAND_CreateWindowSurface(HWND hwnd, BOOL layered, const RECT *surface_re
  */
 BOOL WAYLAND_HasWindowManager(const char *name)
 {
-    const char *env;
+    static int once;
+    const char *env = getenv("XDG_CURRENT_DESKTOP");
+
+    if (!once++)
+        TRACE("DE: %s\n", debugstr_a(env));
+
     if (!strcmp("waylanddrv", name)) return TRUE;
-
-    env = getenv("XDG_CURRENT_DESKTOP");
-    TRACE("DE: %s\n", debugstr_a(env));
-
     if (env && !strcmp(env, name)) return TRUE;
 
     return FALSE;
