251dfbacfc3c510711e3a335a91c0bd3bd7d0b19
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2025-07-17 09:50:37 -0400

    HACK: kernelbase: winewayland: Use --in-process-gpu for EA App.

--- dlls/kernelbase/process.c
+++ dlls/kernelbase/process.c
@@ -626,7 +626,8 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
     static const struct option wayland_options[] = {
         {L"launcher_epic.exe", L" --in-process-gpu"}, /* ZZZ EGS */
         {L"Battle.net.exe", L" --in-process-gpu"},
-        {L"RSI Launcher.exe", L" --in-process-gpu"}
+        {L"RSI Launcher.exe", L" --in-process-gpu"},
+        {L"EADesktop.exe", L" --in-process-gpu"}
     };
 
     static const struct option wow64_options[] = {
@@ -643,7 +644,7 @@ static const WCHAR *hack_append_command_line( const WCHAR *cmd )
     GetEnvironmentVariableA("WINE_WAYLAND_HACKS",
         wayland_hack_enabled, sizeof(wayland_hack_enabled));
 
-    if (wayland_hack_enabled[0] == '1')
+    if (strcmp(wayland_hack_enabled, "1") == 0)
     {
         for (i = 0; i < ARRAY_SIZE(wayland_options); ++i)
         {
