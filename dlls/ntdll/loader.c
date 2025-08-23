abff53c1797deca675383be7a1d713c3318d8140
Stelios Tsampas <loathingkernel@gmail.com>
2025-08-23 16:50:04 +0300

    ntsync: adjust ntsync logging to stop Mangohud from getting confused

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -4748,8 +4748,6 @@ void loader_init( CONTEXT *context, void **entry )
             if (FIXME_ON(sync))
             {
                 NTSTATUS sync = __wine_get_sync_type();
-                if (sync < 0)
-                    MESSAGE("ntsync: explicitly disabled.\n");
                 switch (abs(sync))
                 {
                 case 1:
@@ -4759,12 +4757,14 @@ void loader_init( CONTEXT *context, void **entry )
                     MESSAGE("fsync: up and running.\n");
                     break;
                 case 3:
-                    MESSAGE("wine: using fast synchronization.\n");
+                    MESSAGE("ntsync: up and running.\n");
                     break;
                 default:
                     MESSAGE("wineserver: using server-side synchronization.\n");
                     break;
                 }
+                if (sync < 0)
+                    MESSAGE("ntsync: explicitly disabled.\n");
             }
             if (FIXME_ON(winediag))
             {
