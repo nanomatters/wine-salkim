d54ac28553bf80f4506d51f0a97f2c003963bd03
Louis Lenders <xerox.xerox2000x@gmail.com>
2025-09-06 10:26:11 +0200

    iphlpapi: Add stub for GetPerTcp6ConnectionEStats.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=58635

--- dlls/iphlpapi/iphlpapi_main.c
+++ dlls/iphlpapi/iphlpapi_main.c
@@ -4194,6 +4194,19 @@ ULONG WINAPI GetPerTcpConnectionEStats(MIB_TCPROW *row, TCP_ESTATS_TYPE stats, U
     return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
+/***********************************************************************
+ *    GetPerTcp6ConnectionEStats (IPHLPAPI.@)
+ */
+ULONG WINAPI GetPerTcp6ConnectionEStats(MIB_TCP6ROW *row, TCP_ESTATS_TYPE stats, UCHAR *rw, ULONG rw_version,
+                                        ULONG rw_size, UCHAR *ro_static, ULONG ro_static_version,
+                                        ULONG ro_static_size, UCHAR *ro_dynamic, ULONG ro_dynamic_version,
+                                        ULONG ro_dynamic_size)
+{
+    FIXME( "(%p, %d, %p, %ld, %ld, %p, %ld, %ld, %p, %ld, %ld): stub\n", row, stats, rw, rw_version, rw_size,
+           ro_static, ro_static_version, ro_static_size, ro_dynamic, ro_dynamic_version, ro_dynamic_size );
+    return ERROR_CALL_NOT_IMPLEMENTED;
+}
+
 /******************************************************************
  *    SetPerTcpConnectionEStats (IPHLPAPI.@)
  */
