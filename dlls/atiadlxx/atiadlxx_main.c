2affd1f80375c52aed9c324a64eb5972e612d5c4
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-10-17 16:42:15 +0300

    atiadlxx: Add stub for ADL[2]_Display_SLSMapIndex_Get.

--- dlls/atiadlxx/atiadlxx_main.c
+++ dlls/atiadlxx/atiadlxx_main.c
@@ -1150,3 +1150,25 @@ int CDECL ADL_Display_ODClockInfo_Get(int adapter_index, ADLAdapterODClockInfo *
     clock_info->sEngineClock = clock_info->sMemoryClock;
     return ADL_OK;
 }
+
+int CDECL ADL2_Display_SLSMapIndex_Get(ADL_CONTEXT_HANDLE ctx, int adapter_index, int display_target_count, ADLDisplayTarget *display_target,
+        int *sls_index)
+{
+    TRACE("ctx %p, adapter_index %d, display_target_count %d, display_target %p, sls_index %p.\n",
+            ctx, adapter_index, display_target_count, display_target, sls_index);
+
+    if (!ctx || !display_target || !sls_index) return ADL_ERR_INVALID_PARAM;
+
+    *sls_index = 0;
+
+    return ADL_OK;
+}
+
+int CDECL ADL_Display_SLSMapIndex_Get(int adapter_index, int display_target_count, ADLDisplayTarget *display_target,
+        int *sls_index)
+{
+    TRACE("adapter_index %d, display_target_count %d, display_target %p, sls_index %p.\n",
+            adapter_index, display_target_count, display_target, sls_index);
+
+    return ADL2_Display_SLSMapIndex_Get(default_ctx, adapter_index, display_target_count, display_target, sls_index);
+}
