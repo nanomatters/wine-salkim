1e5608d85cfd072c89a7a7b8aad754a78556721c
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2025-10-22 15:30:05 -0500

    atiadlxx: Add stub for ADL[2]_Display_SLSMapIndex_Get.

--- dlls/atiadlxx/atiadlxx_main.c
+++ dlls/atiadlxx/atiadlxx_main.c
@@ -1065,8 +1065,9 @@ int CDECL ADL_Graphics_Platform_Get(int *platform)
     return ADL2_Graphics_Platform_Get(default_ctx, platform);
 }
 
-int CDECL ADL2_Display_DisplayMapConfig_Get(ADL_CONTEXT_HANDLE ctx, int adapter_index, int *display_map_count, ADLDisplayMap **display_maps,
-        int *display_target_count, ADLDisplayTarget **display_targets, int options)
+int CDECL ADL2_Display_DisplayMapConfig_Get(ADL_CONTEXT_HANDLE ctx, int adapter_index, int *display_map_count,
+                                            ADLDisplayMap **display_maps, int *display_target_count,
+                                            ADLDisplayTarget **display_targets, int options)
 {
     struct gpu *gpu;
     int i;
@@ -1177,4 +1178,26 @@ int CDECL ADL2_OverdriveN_Temperature_Get(ADL_CONTEXT_HANDLE ctx, int adapter_in
 
     *temperature = 65000; /* 65C */
     return ADL_OK;
+}
+
+int CDECL ADL2_Display_SLSMapIndex_Get(ADL_CONTEXT_HANDLE ctx, int adapter_index, int num_display_target,
+                                       ADLDisplayTarget *display_target, int *sls_index)
+{
+    FIXME("ctx %p adapter_index %d num_display_target %d display_target %p sls_index %p stub!\n",
+          ctx, adapter_index, num_display_target, display_target, sls_index);
+
+    if (!ctx || !display_target || !sls_index) return ADL_ERR_INVALID_PARAM;
+
+    *sls_index = 0;
+
+    return ADL_OK;
+}
+
+int CDECL ADL_Display_SLSMapIndex_Get(int adapter_index, int num_display_target,
+                                      ADLDisplayTarget *display_target, int *sls_index)
+{
+    TRACE("adapter_index %d num_display_target %d display_target %p sls_index %p\n",
+          adapter_index, num_display_target, display_target, sls_index);
+
+    return ADL2_Display_SLSMapIndex_Get(default_ctx, adapter_index, num_display_target, display_target, sls_index);
 }
