cfbfdf25d1e7c9a8eb53e4713bb93019ec56b8b6
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2026-01-05 01:40:28 -0500

    winewayland: Don't store numlock/capslock states in keyboard structure.

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -121,8 +121,6 @@ struct wayland_keyboard
     struct wl_keyboard *wl_keyboard;
     struct xkb_context *xkb_context;
     struct xkb_state *xkb_state;
-    BOOL numlock_active;
-    BOOL caplock_active;
     HWND focused_hwnd;
     pthread_mutex_t mutex;
 };
