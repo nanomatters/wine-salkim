c3d26ddfd4e7dc67f26cea75d501f7cd70e5ac3d
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2026-02-10 23:30:27 -0600

    winewayland: Implement support for server-side decorations.

--- dlls/winewayland.drv/waylanddrv_main.c
+++ dlls/winewayland.drv/waylanddrv_main.c
@@ -55,6 +55,7 @@ static const struct user_driver_funcs waylanddrv_funcs =
     .pWindowPosChanged = WAYLAND_WindowPosChanged,
     .pWindowPosChanging = WAYLAND_WindowPosChanging,
     .pCreateWindowSurface = WAYLAND_CreateWindowSurface,
+    .pGetWindowStyleMasks = WAYLAND_GetWindowStyleMasks,
     .pHasWindowManager = WAYLAND_HasWindowManager,
     .pVulkanInit = WAYLAND_VulkanInit,
     .pOpenGLInit = WAYLAND_OpenGLInit,
