d0f3160bd00ea1a0fd73ffdaf1ea95d293817bd2
Alexandre Julliard <julliard@winehq.org>
2026-03-16 13:28:30 +0100

    ntdll: Disable syscall dispatch when loading a .so dll.

--- dlls/ntdll/unix/unix_private.h
+++ dlls/ntdll/unix/unix_private.h
@@ -347,6 +347,7 @@ extern void signal_init_threading(void);
 extern NTSTATUS signal_alloc_thread( TEB *teb );
 extern void set_thread_teb( TEB *teb );
 extern void signal_free_thread( TEB *teb );
+extern void signal_disable_syscall_dispatch(void);
 extern void signal_init_process(void);
 extern void DECLSPEC_NORETURN signal_start_thread( PRTL_THREAD_START_ROUTINE entry, void *arg,
                                                    BOOL suspend, TEB *teb );
