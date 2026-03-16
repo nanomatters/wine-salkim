cbc202e09e87972bbf24a770b1b5c587119266d8
Hans Leidekker <hans@codeweavers.com>
2026-03-16 13:26:35 +0100

    include: Update definition of CERT_CHAIN_ENGINE_CONFIG.
    And fix spelling of hExclusiveTrustedPeople.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=59531
    (cherry picked from commit 2012949a0de0b550d221c5514f5632efcd8c3df2)

--- include/wincrypt.h
+++ include/wincrypt.h
@@ -3503,7 +3503,8 @@ typedef struct _CERT_CHAIN_ENGINE_CONFIG
     DWORD       MaximumCachedCertificates;
     DWORD       CycleDetectionModulus;
     HCERTSTORE  hExclusiveRoot;
-    HCERTSTORE  hExclusiveRootTrustedPeople;
+    HCERTSTORE  hExclusiveTrustedPeople;
+    DWORD       dwExclusiveFlags;
 } CERT_CHAIN_ENGINE_CONFIG, *PCERT_CHAIN_ENGINE_CONFIG;
 
 /* message-related definitions */
