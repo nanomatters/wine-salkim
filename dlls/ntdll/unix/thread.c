1f7be8f57ad6e76b61e79d8dc74b03ab75eb4535
Alexandre Julliard <julliard@winehq.org>
2026-04-22 16:23:45 +0200

    ntdll: Store the current process id in a global variable.

--- dlls/ntdll/unix/thread.c
+++ dlls/ntdll/unix/thread.c
@@ -1181,7 +1181,7 @@ void *get_cpu_area( USHORT machine )
 /***********************************************************************
  *           set_thread_id
  */
-void set_thread_id( TEB *teb, DWORD pid, DWORD tid )
+void set_thread_id( TEB *teb, DWORD tid )
 {
     WOW_TEB *wow_teb = get_wow_teb( teb );
 
@@ -1427,7 +1427,7 @@ NTSTATUS WINAPI NtCreateThreadEx( HANDLE *handle, ACCESS_MASK access, OBJECT_ATT
         goto done;
     }
 
-    set_thread_id( teb, GetCurrentProcessId(), tid );
+    set_thread_id( teb, tid );
 
     teb->SkipThreadAttach = !!(flags & THREAD_CREATE_FLAGS_SKIP_THREAD_ATTACH);
     teb->SkipLoaderInit = !!(flags & THREAD_CREATE_FLAGS_SKIP_LOADER_INIT);
@@ -1555,7 +1555,7 @@ NTSTATUS WINAPI PsCreateSystemThread( HANDLE *handle, ACCESS_MASK access, OBJECT
         goto done;
     }
 
-    set_thread_id( teb, GetCurrentProcessId(), tid );
+    set_thread_id( teb, tid );
 
     thread_data = (struct ntdll_thread_data *)&teb->GdiTebBatch;
     thread_data->request_fd   = request_pipe[1];
@@ -2187,7 +2187,7 @@ static void set_native_thread_name( HANDLE handle, const UNICODE_STRING *name )
     if (NtQueryInformationThread( handle, ThreadBasicInformation, &info, sizeof(info), NULL ))
         return;
 
-    if (HandleToULong( info.ClientId.UniqueProcess ) != GetCurrentProcessId())
+    if (HandleToULong( info.ClientId.UniqueProcess ) != pid )
     {
         static int once;
         if (!once++) FIXME("cross-process native thread naming not supported\n");
@@ -2246,7 +2246,7 @@ static BOOL is_process_wow64( const CLIENT_ID *id )
     ULONG_PTR info;
     BOOL ret = FALSE;
 
-    if (id->UniqueProcess == ULongToHandle(GetCurrentProcessId())) return is_old_wow64();
+    if (id->UniqueProcess == ULongToHandle(pid)) return is_old_wow64();
     if (!NtOpenProcess( &handle, PROCESS_QUERY_LIMITED_INFORMATION, NULL, id ))
     {
         if (!NtQueryInformationProcess( handle, ProcessWow64Information, &info, sizeof(info), NULL ))
