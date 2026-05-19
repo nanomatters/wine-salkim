1e9fa17b7b434151c01e1f254a00d7b470cac843
Stelios Tsampas <loathingkernel@gmail.com>
2026-05-19 15:55:27 +0300

    HACK: kernelbase: remove loaddll_optiscaler_hack from LoadLibraryExW

--- dlls/kernelbase/loader.c
+++ dlls/kernelbase/loader.c
@@ -612,23 +612,6 @@ BOOL loaddll_redirect(LPCWSTR name, LPWSTR override, DWORD size)
     return FALSE;
 }
 
-BOOL loaddll_optiscaler_hack(LPCWSTR name, LPWSTR override, DWORD size)
-{
-    WCHAR dllW[64] = {0};
-    UINT ret;
-
-    ret = GetEnvironmentVariableW( L"WINE_OPTISCALER_NAME", dllW, sizeof(dllW));
-    if ( !ret || ret >= ARRAY_SIZE(dllW) ) return FALSE;
-
-    if ( !wcscmp( name, dllW ) )
-    {
-        swprintf( override, size, L"c:\\windows\\system32\\umu\\%s", dllW );
-        return TRUE;
-    }
-
-    return FALSE;
-}
-
 BOOL loaddll_upscaler_hack(LPCWSTR name, LPWSTR override, DWORD size)
 {
     WCHAR envW[64] = {0};
@@ -691,11 +674,8 @@ HMODULE WINAPI DECLSPEC_HOTPATCH LoadLibraryExW( LPCWSTR name, HANDLE file, DWOR
         flags = 0;
     }
 
-    if ( loaddll_optiscaler_hack(name, overrideW, ARRAY_SIZE(overrideW))
-        || loaddll_upscaler_hack(name, overrideW, ARRAY_SIZE(overrideW)) )
-    {
-        FIXME( "HACK: replacing %s with %s\n", debugstr_w(name), debugstr_w(overrideW));
-    }
+    if ( loaddll_upscaler_hack(name, overrideW, ARRAY_SIZE(overrideW)) )
+        FIXME( "HACK: redirecting %s to %s\n", debugstr_w(name), debugstr_w(overrideW));
 
     RtlInitUnicodeString( &str, overrideW[0] ? overrideW : name );
     if (str.Length && str.Buffer[str.Length/sizeof(WCHAR) - 1] != ' ') return load_library( &str, flags );
