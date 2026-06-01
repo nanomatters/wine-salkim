6de8fdf1686ce137f77cf98ea7d28f5962464926
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2026-06-01 03:19:25 -0400

    win32u: Unconditionally flush the surface on winewayland.
    This was the old behavior before this function was introduced, some drivers behave in slightly different ways and now standardizing it has caused issues for winewayland.

--- dlls/win32u/window.c
+++ dlls/win32u/window.c
@@ -2504,7 +2504,8 @@ static BOOL expose_window_surface( HWND hwnd, UINT flags, const RECT *rect, UINT
         add_bounds_rect( &surface->bounds, &exposed_rect );
     }
     window_surface_unlock( surface );
-    if (surface->alpha_mask) window_surface_flush( surface );
+    if (surface->alpha_mask || !user_driver->dc_funcs.pPutImage)
+        window_surface_flush( surface );
     window_surface_release( surface );
     return TRUE;
 }
