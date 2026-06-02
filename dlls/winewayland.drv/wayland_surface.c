42073195455e1d6c74c5e7b7b9bc7f1dcefdf0b8
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2026-06-01 23:05:52 -0400

    winewayland: Properly unset the client surface when attach fails.

--- dlls/winewayland.drv/wayland_surface.c
+++ dlls/winewayland.drv/wayland_surface.c
@@ -2109,12 +2109,12 @@ static void wayland_client_surface_update(struct client_surface *client)
 
     if (!(data = wayland_win_data_get(hwnd))) return;
 
-    if (data->client_surface != surface)
+    if (toplevel && NtUserIsWindowVisible(hwnd))
+        wayland_client_surface_attach(surface, toplevel);
+    else
     {
-        if (toplevel && NtUserIsWindowVisible(hwnd))
-            wayland_client_surface_attach(surface, toplevel);
-        else
-            wayland_client_surface_attach(surface, NULL);
+        wayland_client_surface_attach(surface, NULL);
+        data->client_surface = NULL;
     }
 
     wayland_win_data_release(data);
