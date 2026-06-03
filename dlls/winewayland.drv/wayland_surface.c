617273ddee437598312abad3e40f4db2d10cb7f6
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2026-06-02 20:00:51 -0400

    winewayland: Add missing traces for client surfaces.

--- dlls/winewayland.drv/wayland_surface.c
+++ dlls/winewayland.drv/wayland_surface.c
@@ -2089,6 +2089,8 @@ static void wayland_client_surface_detach(struct client_surface *client)
     struct wayland_client_surface *surface = impl_from_client_surface(client);
     struct wayland_win_data *data;
 
+    TRACE("%s\n", debugstr_client_surface(client));
+
     if ((data = wayland_win_data_get(client->hwnd)))
     {
         if (data->client_surface == surface) data->client_surface = NULL;
@@ -2102,6 +2104,8 @@ static void wayland_client_surface_update(struct client_surface *client)
     struct wayland_client_surface *surface = impl_from_client_surface(client);
     HWND hwnd = client->hwnd, toplevel = NtUserGetAncestor(hwnd, GA_ROOT);
     struct wayland_win_data *data;
+
+    TRACE("%s\n", debugstr_client_surface(client));
 
     if (!(data = wayland_win_data_get(hwnd))) return;
 
@@ -2240,6 +2244,8 @@ void wayland_client_surface_attach(struct wayland_client_surface *client, HWND t
         wl_subsurface_set_desync(client->wl_subsurface);
 
         client->toplevel = toplevel;
+
+        TRACE("Created subsurface for toplevel=%p\n", toplevel);
     }
 
     NtUserGetClientRect(hwnd, &client_rect, NtUserGetWinMonitorDpi(hwnd, MDT_RAW_DPI));
