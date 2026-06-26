63c8cdc5ba1921123c666e461639cb5ed4615d88
Erhan Bilgili <erhan.bilgili@gmail.com>
2026-06-26 22:20:59 +0200

    winewayland: Use linux-dmabuf feedback for dmabuf caps

--- dlls/winewayland.drv/vulkan.c
+++ dlls/winewayland.drv/vulkan.c
@@ -123,16 +123,21 @@ static UINT wayland_vulkan_get_hwnd_dmabuf_caps(HWND hwnd, void *caps_ptr, void 
         if (presentable_locally) return HWND_DMABUF_NOT_FOUND;
     }
 
-    wl_list_for_each(entry, &process_wayland.dmabuf_formats, link) count++;
+    pthread_mutex_lock(&process_wayland.dmabuf_mutex);
+    wl_list_for_each(entry, &process_wayland.dmabuf_formats, link)
+        count++;
 
     memset(caps, 0, sizeof(*caps));
     wl_list_for_each(entry, &process_wayland.dmabuf_formats, link)
     {
         if (copied >= max_format_modifiers) break;
         format_modifiers[copied].fourcc = entry->format;
+        format_modifiers[copied].tranche_index = entry->tranche_index;
+        format_modifiers[copied].tranche_flags = entry->tranche_flags;
         format_modifiers[copied].modifier = entry->modifier;
         copied++;
     }
+    pthread_mutex_unlock(&process_wayland.dmabuf_mutex);
 
     caps->format_modifier_count = copied;
     *format_modifier_count = count;
--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -171,6 +171,34 @@ struct wayland_data_device
     pthread_mutex_t mutex;
 };
 
+struct wayland_dmabuf_format
+{
+    struct wl_list link;
+    uint32_t format;
+    uint32_t tranche_index;
+    uint32_t tranche_flags;
+    uint64_t modifier;
+};
+
+#define DRM_FORMAT_MOD_INVALID 0x00ffffffffffffffull
+
+struct wayland_dmabuf_feedback_format
+{
+    uint32_t format;
+    uint32_t padding;
+    uint64_t modifier;
+};
+
+struct wayland_dmabuf_feedback
+{
+    struct zwp_linux_dmabuf_feedback_v1 *zwp_linux_dmabuf_feedback_v1;
+    struct wayland_dmabuf_feedback_format *format_table;
+    size_t format_table_count;
+    struct wl_list pending_formats;
+    uint32_t tranche_index;
+    uint32_t tranche_flags;
+};
+
 struct wayland
 {
     BOOL initialized;
@@ -184,7 +212,9 @@ struct wayland
     struct wp_viewporter *wp_viewporter;
     struct wl_subcompositor *wl_subcompositor;
     struct zwp_linux_dmabuf_v1 *zwp_linux_dmabuf_v1;
+    struct wayland_dmabuf_feedback dmabuf_default_feedback;
     struct wl_list dmabuf_formats;
+    pthread_mutex_t dmabuf_mutex;
     struct wp_fractional_scale_manager_v1 *wp_fractional_scale_manager_v1;
     struct wp_alpha_modifier_v1 *wp_alpha_modifier_v1;
     struct zwlr_layer_shell_v1 *zwlr_layer_shell_v1;
@@ -224,15 +254,6 @@ struct wayland_output_state
     int logical_x, logical_y;
     int logical_w, logical_h;
 };
-
-struct wayland_dmabuf_format
-{
-    struct wl_list link;
-    uint32_t format;
-    uint64_t modifier;
-};
-
-#define DRM_FORMAT_MOD_INVALID 0x00ffffffffffffffull
 
 struct wayland_output
 {
